9c04c1c4897a3a3164a3c947f2b8d34807e7c010
Aleksandr Litvinov <ssssank@gmail.com>
2023-08-16 10:48:51 +0300

    add templates

--- public/index.php
+++ public/index.php
@@ -3,8 +3,15 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
+$container = new Container();
+
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
@@ -27,6 +34,11 @@ $app->post('/domains', function ($request, $response) {
 $app->get('/courses/{id}', function ($request, $response, array $args) {
     $id = $args['id'];
     return $response->write("Course id: {$id}");
+});
+
+$app->get('/users/{id}', function ($request, $response, $args) {
+    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
 });
 
 $app->run();
